8e37f6c158b36269e0e97f815b818981a6fec0a6
Tabaré Caorsi <tabare@heapstersoft.com>
2014-09-27 11:23:28 -0300

    Don't add field named 'created' or 'updated' to forms

--- Command/GenerateCrudCommand.php
+++ Command/GenerateCrudCommand.php
@@ -9,11 +9,15 @@ namespace p1r0\GeneratorBundle\Command;
 
 use Sensio\Bundle\GeneratorBundle\Command\GenerateDoctrineCrudCommand;
 use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
+use Sensio\Bundle\GeneratorBundle\Generator\DoctrineFormGenerator;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
 class GenerateCrudCommand extends GenerateDoctrineCrudCommand
 {
     protected $generator;
+    protected $formGenerator;
+
+    protected $tplDirs;
 
     protected function configure()
     {
@@ -23,16 +27,33 @@ class GenerateCrudCommand extends GenerateDoctrineCrudCommand
     protected function getGenerator(BundleInterface $bundle = null)
     {
         if (null === $this->generator) {
-            $kernel = $this->getContainer()->get('kernel');
-            $path = $kernel->locateResource('@SensioGeneratorBundle/Resources/SensioGeneratorBundle/skeleton');
-            $directories = array(
-                $path
-            );
-
             $this->generator = $this->createGenerator();
-            $this->generator->setSkeletonDirs($directories);
+            $this->generator->setSkeletonDirs($this->getTemplateDirs());
         }
 
         return $this->generator;
+    }
+
+    protected function getFormGenerator($bundle = null)
+    {
+        if (null === $this->formGenerator) {
+            $this->formGenerator = new DoctrineFormGenerator($this->getContainer()->get('filesystem'));
+            $this->formGenerator->setSkeletonDirs($this->getTemplateDirs());
+        }
+
+        return $this->formGenerator;
+    }
+
+    protected function getTemplateDirs()
+    {
+        if (empty($this->tplDirs)) {
+            $kernel = $this->getContainer()->get('kernel');
+            $path = $kernel->locateResource('@SensioGeneratorBundle/Resources/SensioGeneratorBundle/skeleton');
+            $this->tplDirs = array(
+                $path
+            );
+        }
+
+        return $this->tplDirs;
     }
 }
